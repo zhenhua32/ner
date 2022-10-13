ff332c2a586e9d00a4696ecb299d334cbc596508
zhenhua32 <zhenhua32@gmail.com>
2022-10-13 23:02:47 +0800

    换名字

--- torch_version/torch_model_lstm_crf.py
+++ torch_version/torch_model_lstm_crf.py
@@ -4,12 +4,13 @@ import time
 
 import sys
 
-root_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print(root_path)
 sys.path.append(root_path)
 
 import torch
 import torch.nn as nn
+import torch.optim as optim
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
@@ -32,6 +33,9 @@ test_output_file = os.path.join(root_path, "./data/test/output.seq.bioattr")
 class MyDataset(Dataset):
     def __init__(self, input_seq_path: str, output_seq_path: str, w2i_char: dict, w2i_bio: dict) -> None:
         super().__init__()
+        print("加载 ", input_seq_path)
+        print("加载 ", output_seq_path)
+
         inputs_seq = []
         with open(input_seq_path, "r", encoding="utf-8") as f:
             for line in f.read().strip().split("\n"):
@@ -47,6 +51,7 @@ class MyDataset(Dataset):
                 outputs_seq.append(seq)
 
         # 简单验证下数据, 总行数要相同, 每行的单词数也要相同
+        print("行数", len(inputs_seq), len(outputs_seq))
         assert len(inputs_seq) == len(outputs_seq)
         assert all(len(input_seq) == len(output_seq) for input_seq, output_seq in zip(inputs_seq, outputs_seq))
 
@@ -79,16 +84,16 @@ def collate_fn(batch_data, w2i_char=w2i_char, w2i_bio=w2i_bio):
         seq.extend([w2i_bio["O"]] * (max_seq_len - len(seq)))
 
     return (
-        torch.tensor(inputs_seq_batch, dtype=torch.int32),
-        torch.tensor(outputs_seq_batch, dtype=torch.int32),
-        torch.tensor(inputs_seq_len_batch, dtype=torch.int32),
+        torch.tensor(inputs_seq_batch),
+        torch.tensor(outputs_seq_batch),
+        torch.tensor(inputs_seq_len_batch),
     )
 
 
-train_dataset = MyDataset(train_input_file, test_output_file, w2i_char, w2i_bio)
+train_dataset = MyDataset(train_input_file, train_output_file, w2i_char, w2i_bio)
 test_dataset = MyDataset(test_input_file, test_output_file, w2i_char, w2i_bio)
-train_dataloader = DataLoader(train_dataset, batch_size=128, shuffle=True, collate_fn=collate_fn)
-test_dataloader = DataLoader(test_dataset, batch_size=128, shuffle=False, collate_fn=collate_fn)
+train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=True, collate_fn=collate_fn)
+test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False, collate_fn=collate_fn)
 
 
 # 创建模型
@@ -101,29 +106,68 @@ class MyModel(nn.Module):
         self.lstm = nn.LSTM(
             input_size=embedding_dim,
             hidden_size=hidden_size,
+            batch_first=True,
             num_layers=1,
             bidirectional=True,
         )
-        self.linear = nn.Linear(hidden_size, output_size)
+        self.linear = nn.Linear(2 * hidden_size, output_size)
         self.hidden_size = hidden_size
+        self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, x):
         batch_size = x.shape[0]
         seq_len = x.shape[1]
         emb = self.embedding(x)
-        h0 = Variable(torch.zeros(seq_len, batch_size, self.hidden_size))
-        c0 = Variable(torch.zeros(seq_len, batch_size, self.hidden_size))
+        h0 = Variable(torch.zeros(2, batch_size, self.hidden_size)).to(device)
+        c0 = Variable(torch.zeros(2, batch_size, self.hidden_size)).to(device)
         output, (final_hidden_state, final_cell_state) = self.lstm(emb, (h0, c0))
-        return self.linear(final_hidden_state[-1])
+        output = self.linear(output)
+        output = self.softmax(output)
+        return output
 
 
-device = "cuda" if torch.cuda.is_gpu_available() else "cpu"
+device = "cuda" if torch.cuda.is_available() else "cpu"
 model = MyModel()
 model.to(device)
+print(model)
 
 loss_fn = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
 
-def train(model, dataloader, loss_fn, optimizer):
-    pass
+def train(dataloader: DataLoader, model: MyModel, loss_fn: nn.CrossEntropyLoss, optimizer: optim.Adam):
+    model.train()
+    size = len(dataloader.dataset)
+    for batch, (inputs_seq_batch, outputs_seq_batch, inputs_seq_len_batch) in enumerate(dataloader):
+        inputs_seq_batch = inputs_seq_batch.to(device)
+        outputs_seq_batch = outputs_seq_batch.to(device)
+        inputs_seq_len_batch = inputs_seq_len_batch.to(device)
+        pred = model(inputs_seq_batch)
+        # print("inputs_seq_batch", inputs_seq_batch.shape)
+        # print("pred", pred.shape)
+        # print("outputs_seq_batch", outputs_seq_batch.shape, outputs_seq_batch.dtype)
+        # print("pred", pred.shape, pred.dtype)
+        # print(pred)
+        # print(outputs_seq_batch)
+        # 求损失的时候, 需要形状是 (N, C, other) 的, N 是 batch_size, C 是类别数
+        pred = torch.transpose(pred, 1, 2)
+        # print("pred", pred.shape, pred.dtype)
+        loss = loss_fn(pred, outputs_seq_batch)
 
+        # 反向传播
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(inputs_seq_batch)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+
+epochs = 5
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(train_dataloader, model, loss_fn, optimizer)
+    # test(test_dataloader, model, loss_fn)
+print("Done!")
