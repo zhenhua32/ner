dd5bebb31e72c4f67d08413d406d2d82bbe678d7
zhenhua32 <zhenhua32@gmail.com>
2023-03-07 22:50:07 +0800

    不管咋调, crf 都更差

--- torch_version/train_lstm.py
+++ torch_version/train_lstm.py
@@ -1,9 +1,11 @@
 import os
+import time
 from functools import partial
 
 import torch
 from torch.utils.data import DataLoader
 import transformers
+from lion_pytorch import Lion
 
 from utils_tools import load_vocabulary
 from data_tools import set_seed, LSTMDataset, padding_to_batch_max_len, calc_weight_dict
@@ -31,13 +33,13 @@ test_output_file = None
 collate_fn = partial(padding_to_batch_max_len, w2i_char=w2i_char, w2i_bio=w2i_bio)
 train_dataset = LSTMDataset(train_input_file, train_output_file, w2i_char, w2i_bio)
 test_dataset = LSTMDataset(test_input_file, test_output_file, w2i_char, w2i_bio)
-train_dataloader = DataLoader(train_dataset, batch_size=128, shuffle=True, collate_fn=collate_fn)
-test_dataloader = DataLoader(test_dataset, batch_size=128, shuffle=False, collate_fn=collate_fn)
+train_dataloader = DataLoader(train_dataset, batch_size=512, shuffle=True, collate_fn=collate_fn, pin_memory=True)
+test_dataloader = DataLoader(test_dataset, batch_size=512, shuffle=False, collate_fn=collate_fn, pin_memory=True)
 
 weights = calc_weight_dict(train_dataset, i2w_bio, device)
 
 # flag
-use_crf = True
+use_crf = False
 
 if use_crf:
     model = LSTMCRFModel(
@@ -61,15 +63,28 @@ epochs = 20
 max_train_steps = epochs * len(train_dataloader)
 lr_warmup_steps = max_train_steps // 20
 
-optimizer = torch.optim.Adam(model.parameters(), lr=5e-3)
-# scheduler = transformers.get_cosine_with_hard_restarts_schedule_with_warmup(
-#     optimizer=optimizer,
-#     num_warmup_steps=lr_warmup_steps,
-#     num_training_steps=max_train_steps,
-#     num_cycles=2,
-# )
+# 分组参数
+crf_params = [param for name, param in model.named_parameters() if "crf" in name]
+other_params = [param for name, param in model.named_parameters() if "crf" not in name]
+
+optimizer = torch.optim.AdamW(
+    [
+        {"params": crf_params, "lr": 5e-2},
+        {"params": other_params, "lr": 1e-2},
+    ],
+    weight_decay=0.01,
+)
 if use_crf:
-    # crf 的起步也太慢了, 第一轮的结果就比较差
+    # TODO: crf 的起步也太慢了, 第一轮的结果就比较差
+    scheduler = transformers.get_cosine_schedule_with_warmup(
+        optimizer=optimizer,
+        num_warmup_steps=0,
+        num_training_steps=max_train_steps,
+        num_cycles=0.5,
+    )
+    # scheduler = None
+else:
+    # 对于 lstm 来说, 不需要 warmup, 第一个 epoch 就比较高了
     # scheduler = transformers.get_cosine_schedule_with_warmup(
     #     optimizer=optimizer,
     #     num_warmup_steps=0,
@@ -77,20 +92,15 @@ if use_crf:
     #     num_cycles=0.5,
     # )
     scheduler = None
-else:
-    # 对于 lstm 来说, 不需要 warmup, 第一个 epoch 就比较高了
-    scheduler = transformers.get_cosine_schedule_with_warmup(
-        optimizer=optimizer,
-        num_warmup_steps=0,
-        num_training_steps=max_train_steps,
-        num_cycles=0.5,
-    )
 
 best_f1 = 0
 for t in range(epochs):
+    start = time.time()
     print(f"Epoch {t+1}\n-------------------------------")
     train(train_dataloader, model, optimizer, device)
+    print("train time: ", time.time() - start)
     p, r, f1 = test(test_dataloader, model, device, use_crf, i2w_char, i2w_bio)
+    print("eval time: ", time.time() - start)
     if f1 > best_f1:
         # 保存模型
         model_file_name = "lstm_model.pt" if not use_crf else "lstm_crf_model.pt"
@@ -100,5 +110,6 @@ for t in range(epochs):
     # 在每轮结束后调整学习率
     if scheduler is not None:
         scheduler.step()
+    print(f"Epoch {t+1} cost {time.time() - start:.2f} s")
 print("Done!")
 print("best_f1: ", best_f1)
